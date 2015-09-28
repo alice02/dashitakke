44dc7f1829ce4e9112b6f8851cb272f27595c8dc
Kouta ASAI <scene.4913@gmail.com>
2015-09-28 23:51:31 +0900

    questionモデルに追加したカラムに対応させた

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -73,6 +73,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:index, :need_check, :point, :contents, :paper_id)
+      params.require(:question).permit(:index, :need_check, :point, :contents, :paper_id, :extension, :mime)
     end
 end
